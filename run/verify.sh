7ec59df2f09dcf956974c37e2aafbcc30a841e18
Philip Davis <philipdavis01@gmail.com>
2020-03-31 13:07:13 -0400

    Handle asynchronous output in the put. Maybe fix this with a Barrier as well?

--- run/verify.sh
+++ run/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-diff <(awk 'NR>1 {print $4, $6, $8}' put.log | sort -n) <(awk '{print $4, $5, $7}' get.log | sort -n) > /dev/null
+diff <(awk '!/integers/ {print $4, $6, $8}' put.log | sort -n) <(awk '{print $4, $5, $7}' get.log | sort -n) > /dev/null
 if [ "$?" == "0" ] ; then
     echo "Results OK"
 else
